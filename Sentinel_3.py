c25e75fbbfb325406db9522cc1ef01e3ba5c64bd
gerry <jakob@geriner-j.eu>
2025-11-08 23:42:06 +0100

    sentinel 3 script main

---
+++ Sentinel_3.py
@@ -0,0 +1,44 @@
+import datetime
+
+import openeo
+import xarray
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    conn = openeo.connect("openeo.dataspace.copernicus.eu")
+    conn.authenticate_oidc()
+
+    conn.describe_collection("SENTINEL3_OLCI_L1B")
+
+    today = datetime.datetime.today().strftime('%Y-%m-%d')
+
+    bbox = {"west": 27.564697, "south": 34.764179, "east": 33.002930, "north": 37.387617}
+    sentinel3 = conn.load_collection(
+        "SENTINEL3_OLCI_L1B",
+        spatial_extent=bbox,
+        temporal_extent=[today, today],
+        bands=["B08", "B06", "B04"],
+    )
+
+    sentinel3.download("sentinel3.nc")
+
+    ds = xarray.load_dataset("sentinel3.nc")
+
+    # Convert xarray DataSet to a (bands, t, x, y) DataArray
+    data = ds[["B08", "B06", "B04"]].to_array(dim="bands")
+
+
+    # Create a figure and axis for only the RGB plot
+    fig, axrgb = plt.subplots(figsize=(8, 8))
+
+    # Plot the image
+    data[{"t": 0}].plot.imshow(ax=axrgb)
+
+    # Remove axes, borders, and whitespace
+    axrgb.axis('off')
+    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+
+    # Save the clean image
+    plt.savefig("satellite_images/foo.png", dpi=300, bbox_inches='tight', pad_inches=0)
+    plt.close(fig)
