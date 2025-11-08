284a35f2bbabfb04acce2b7e03f2e518e1b3b493
gerry <jakob@geriner-j.eu>
2025-11-09 00:49:32 +0100

    add data loop

--- processing/Sentinel_3.py
+++ processing/Sentinel_3.py
@@ -1,10 +1,9 @@
-import datetime
-from datetime import timedelta
+from datetime import datetime
 
+import matplotlib.pyplot as plt
 import openeo
 import xarray
-import matplotlib.pyplot as plt
-import sys
+from dateutil.relativedelta import relativedelta
 
 if __name__ == "__main__":
     #west = sys.argv[1]
@@ -14,37 +13,47 @@ if __name__ == "__main__":
 
     conn = openeo.connect("openeo.dataspace.copernicus.eu")
     conn.authenticate_oidc()
-
     conn.describe_collection("SENTINEL3_OLCI_L1B")
 
-    today = datetime.datetime.today()
-
-    bbox = {"west": 27.564697, "south": 34.764179, "east": 33.002930, "north": 37.387617}
-    sentinel3 = conn.load_collection(
-        "SENTINEL3_OLCI_L1B",
-        spatial_extent=bbox,
-        temporal_extent=[today.strftime('%Y-%m-%d'), today.strftime('%Y-%m-%d')],
-        bands=["B08", "B06", "B04"],
-    )
-
-    #sentinel3.download("sentinel3.nc")
-
-    ds = xarray.load_dataset("sentinel3.nc")
-
-    # Convert xarray DataSet to a (bands, t, x, y) DataArray
-    data = ds[["B08", "B06", "B04"]].to_array(dim="bands")
 
 
-    # Create a figure and axis for only the RGB plot
-    fig, axrgb = plt.subplots(figsize=(8, 8))
+    start_date = datetime(2025, 6, 1)
+    end_date = datetime(2025, 11, 1)
 
-    # Plot the image
-    data[{"t": 0}].plot.imshow(ax=axrgb)
+    step = relativedelta(weeks=1)
 
-    # Remove axes, borders, and whitespace
-    axrgb.axis('off')
-    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+    current = start_date
+    while current <= end_date:
+        print(current.strftime("%Y-%m-%d"))
+        current += step
 
-    # Save the clean image
-    plt.savefig("processing/satellite_images/foo.png", dpi=300, bbox_inches='tight', pad_inches=0)
-    plt.close(fig)
+        bbox = {"west": 27.564697, "south": 34.764179, "east": 33.002930, "north": 37.387617}
+
+        sentinel3 = conn.load_collection(
+            "SENTINEL3_OLCI_L1B",
+            spatial_extent=bbox,
+            temporal_extent=[current.strftime('%Y-%m-%d'), current.strftime('%Y-%m-%d')],
+            bands=["B08", "B06", "B04"],
+        )
+
+        sentinel3.download("sentinel3.nc")
+
+        ds = xarray.load_dataset("sentinel3.nc")
+
+        # Convert xarray DataSet to a (bands, t, x, y) DataArray
+        data = ds[["B08", "B06", "B04"]].to_array(dim="bands")
+
+
+        # Create a figure and axis for only the RGB plot
+        fig, axrgb = plt.subplots(figsize=(8, 8))
+
+        # Plot the image
+        data[{"t": 0}].plot.imshow(ax=axrgb)
+
+        # Remove axes, borders, and whitespace
+        axrgb.axis('off')
+        plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+
+        # Save the clean image
+        plt.savefig(f"processing/satellite_images/{current}.png", dpi=300, bbox_inches='tight', pad_inches=0)
+        plt.close(fig)
